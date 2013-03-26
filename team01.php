e3c91c5331777950108c321fcb5cccf8fcf7a013
pavelminsky <pavelminsky@gmail.com>
2013-03-26 16:59:09 +0400

    team - main, 01, crew

--- team01.php
+++ team01.php
@@ -1,5 +1,15 @@
 <?php include ("header_include.php"); ?>
 
+<!-- Team nav -->
+<script type="text/javascript">
+$(document).ready(function(){
+$(".team_showlist").toggle(
+  function () {$('.team_list').fadeIn(); $('.team_person').fadeOut();},
+  function () {$('.team_list').fadeOut(); $('.team_person').fadeIn();}
+  );
+});
+</script>
+
 <link href="/css/video-js.css" rel="stylesheet">
 <script type="text/javascript" src="/js/video.js"></script>
 <style type="text/css">
@@ -32,35 +42,55 @@
 	<a id="menu2" class="fadeout off" href="javascript:void(0)">Новости</a>
 	<a id="menu3" class="fadeout off" href="javascript:void(0)">Книга</a>
 	<a id="menu4" class="fadeout" href="/about/">Фильм</a>
-	<a id="menu5" class="fadeout active" href="/team/">Команда</a>
+	<a id="menu5" class="fadeout active" href="/team/" style="cursor: pointer;">Команда</a>
 	<a id="menu6" class="fadeout" href="/video/">Видео</a>
 	<a id="menu7" class="fadeout" href="/gallery/">Кадры</a>
 </div>
 
 
-
-<style type="text/css">
-
-body#team01 .logo_small {position: absolute; top: 40px; right: 40px; z-index: 8;}
-
-.team_breadcrumbs {position: absolute; top: 77px; left: 40px; line-height: 34px;}
-.team_breadcrumbs a {font-size: 20px; font-weight: 300;}
-.team_breadcrumbs b {display: block; font-size: 42px; font-weight: 400;}
-
-body#team01 .team_breadcrumbs {color: #ede2cb; text-shadow: 0 1px 1px #321700;}
-body#team01 .team_breadcrumbs a, body#team01 .team_breadcrumbs a:visited {color: #ffcb7d;}
-body#team01 .team_breadcrumbs a:hover {color: #ffb74b;}
-
-.superwrap {width: 400px; height: 167px; margin: 0 auto 0 auto; text-align: center;}
-</style>
-
-
 <div class="logo_small noselect">&nbsp;</div>
 
-<div class="team_breadcrumbs"><a href="/team/" class="fadeout">Команда</a><b>Сирша Ронан</b></div>
+<div class="team_showlist">Команда</div>
+<div class="team_person">Сирша Ронан</div>
+
+<div class="team_list" style="display: none;">
+	<a href="/team01/" class="fadeout active">Сирша Ронан</a>
+	<a href="/team02/" class="fadeout">Макс Айронс</a>
+	<a href="/team03/" class="fadeout">Диана Крюгер</a>
+	<a href="/team04/" class="fadeout">Джейк Абель</a>
+	<a href="/team05/" class="fadeout">Чандлер Кентербери</a>
+	<a href="/team06/" class="fadeout">Уильям Хёрт</a>
+	<a href="/team07/" class="fadeout">Фрэнсис Фишер</a>
+	<a href="/teamcrew/" class="fadeout">Эндрю Никкол</a>
+	<a href="/teamcrew/" class="fadeout">Стефани Майер</a>
+</div>
 
 
 
+<div class="team01_wrap clearfix">
+	<div class="team01_lc">
+		<div class="team01_content">
+		<div class="team01_melanie noselect">&nbsp;</div>
+		<h4>...В роли Мелани Страйдер</h4>
+		<p><b>Мелани</b> – сильная, храбрая, решительная девушка. Когда человечество атакует незримый враг, Мелани рискует всем, чтобы защитить своих близких: Джареда, её брата Джейми и дядю Джеба. Она никогда не сдаётся без боя, и её любовь к Джареду позволила Мелани сохранить самосознание и волю даже тогда, когда в её тело поселилась Душа – Анни.</p>
+		</div>
+	</div>
+	<div class="team01_rc">
+		<div class="team01_content">
+		<div class="team01_wanda noselect">&nbsp;</div>
+		<h4>...В роли Анни (Странницы)</h4>
+		<p><b>Анни</b> – Душа, не похожая на остальные. Её опыт и мудрость помогают Страннице найти общий язык с Мелани и понять, что значит быть человеком. А сблизившись с членами повстанческого движения, Анни неожиданно открывает для себя сильнейшую из человеческих эмоций – любовь.</p>
+		</div>
+	</div>
+	<div class="team01_video_wrap clearfix">
+		<div class="team01_lc"><div class="team01_video"><div style="width: 360px; height: 206px; border: 1px solid #ffab67;"><video id="melaniestryder" class="video-js vjs-default-skin" controls preload="auto" width="360" height="206" poster="/video/melaniestryder.jpg" data-setup="{}"><source src="http://www.hostmovie.ru/video/melaniestryder.mp4" type='video/mp4'></div></div></div>
+		<div class="team01_rc"><div class="team01_video"><div style="width: 360px; height: 206px; border: 1px solid #bee8ef;"><video id="wandererwanda" class="video-js vjs-default-skin" controls preload="auto" width="360" height="206" poster="/video/wandererwanda.jpg" data-setup="{}"><source src="http://www.hostmovie.ru/video/wandererwanda.mp4" type='video/mp4'></div></div></div>
+	</div>
+	<div class="team01_mc">
+		<p>Несмотря на свой юный возраст (ей нет и девятнадцати), рождённая в семье ирландских актёров <b>Сирша Ронан</b> успешно работает в кино уже больше десяти лет. Первая же крупная роль в фильме <u>«Искупление»</u> Джо Райта принесла тринадцатилетней девушке номинацию на «Оскар», и с тех пор она успела сняться в таких крупных фильмах, как <u>«Город Эмбер: Побег»</u>, <u>«Ханна. Совершенное оружие»</u> и <u>«Милые кости»</u>.</p>
+		<p>Сейчас она снимается с Биллом Мюрреем в фильме Веса Андерсона <u>«Отель Будапешт»</u>, а вскоре после «Гостьи» на экраны выйдет драма о вампирах Нила Джордана <u>«Византия»</u> с Сиршей в одной из главных ролей.</p>
+	</div>
+</div>
 
 
 
---
+++ teamcrew.php
@@ -0,0 +1,87 @@
+<?php include ("header_include.php"); ?>
+
+<!-- Team nav -->
+<script type="text/javascript">
+$(document).ready(function(){
+$(".team_showlist").toggle(
+  function () {$('.team_list').fadeIn(); $('.team_person').fadeOut();},
+  function () {$('.team_list').fadeOut(); $('.team_person').fadeIn();}
+  );
+});
+</script>
+
+</head><body id="teamcrew">
+
+<noscript><style>.dim {display: none;} #counters:hover {bottom: 0;}</style><div style="position: fixed; bottom: 80px; left: 50%; width: 360px; z-index: 9999; margin-left: -180px; font-size: 14px; color: #849091; line-height: 20px; text-align: center; border: 1px solid #849091; background-color: #000000;">В вашем браузере отключен JavaScript</div></noscript>
+<div class="dim">&nbsp;</div>
+
+
+
+<div class="menu noselect">
+	<a id="menu1" class="fadeout" href="/">Главная</a>
+	<a id="menu2" class="fadeout off" href="javascript:void(0)">Новости</a>
+	<a id="menu3" class="fadeout off" href="javascript:void(0)">Книга</a>
+	<a id="menu4" class="fadeout" href="/about/">Фильм</a>
+	<a id="menu5" class="fadeout active" href="/team/" style="cursor: pointer;">Команда</a>
+	<a id="menu6" class="fadeout" href="/video/">Видео</a>
+	<a id="menu7" class="fadeout" href="/gallery/">Кадры</a>
+</div>
+
+
+<div class="logo_small noselect">&nbsp;</div>
+
+
+<div class="team_showlist">Команда</div>
+<div class="team_person">Эндрю Никкол<br>Стефани Майер</div>
+
+<div class="team_list" style="display: none;">
+	<a href="/team01/" class="fadeout">Сирша Ронан</a>
+	<a href="/team02/" class="fadeout">Макс Айронс</a>
+	<a href="/team03/" class="fadeout">Диана Крюгер</a>
+	<a href="/team04/" class="fadeout">Джейк Абель</a>
+	<a href="/team05/" class="fadeout">Чандлер Кентербери</a>
+	<a href="/team06/" class="fadeout">Уильям Хёрт</a>
+	<a href="/team07/" class="fadeout">Фрэнсис Фишер</a>
+	<a href="/teamcrew/" class="fadeout active">Эндрю Никкол</a>
+	<a href="/teamcrew/" class="fadeout active">Стефани Майер</a>
+</div>
+
+
+<div class="teamcrew_wrap clearfix">
+	<div class="teamcrew_lc">
+		<div class="teamcrew_content">
+		<div class="teamcrew_niccol noselect">&nbsp;</div>
+		<h4>Режиссёр и сценарист</h4>
+		<p><b>Эндрю Никкол</b> – известный сценарист и режиссёр, из-под пера которого неизменно выходят умные, интересные, разнообразные, необычные фантастические картины.</p>
+		<p>Первыми режиссёрскими опытами для Эндрю были съёмки рекламных роликов в Лондоне, но в итоге он захотел «снимать фильмы длиннее шестидесяти секунд» и перебрался в Лос-Анджелес. И уже своим дебютом, антиутопией <u>«Гаттака»</u> с Джадом Лоу, Итанам Хоуком и Умой Турман, Эндрю вывел себя в ряды самых многообещающих новых режиссёров конца XX и начала XXI века. Ну а последовавшие за ней <u>«Шоу Трумана»</u> с Джимом Керри (номинировавшийся на «Оскар» за лучший сценарий), <u>«Симона»</u> с Аль Пачино, <u>«Оружейный барон»</u> с Николасом Кейджем и <u>«Время»</u> с Джастином Тимберлейком лишь утвердили за ним это звание.</p>
+		<p><u>«Гостья»</u> – первый случай в карьере Никкола, когда он не пишет сценарий с нуля, а адаптирует уже имеющееся произведение.</p>
+		</div>
+	</div>
+	<div class="teamcrew_rc">
+		<div class="teamcrew_content">
+		<div class="teamcrew_meyer noselect">&nbsp;</div>
+		<h4>Автор первоисточника</h4>
+		<p>Одна из самых успешных женщин-писательниц, <b>Стефани Майер</b> мгновенно прославилась благодаря своему фантастическому любовному роману <u>«Сумерки»</u>, значительно увеличила свою популярность с выходом трёх сиквелов к этой книге и превратилась в самую настоящую звезду, когда невероятного успеха добились экранизации её романов, получившие общий заголовок <u>«Сумерки. Сага»</u>.</p>
+		<p>Этот её цикл породил самый настоящий бум и в литературе, и в кинематографе, открыв всему миру глаза на огромную женскую аудиторию, жаждавшую ориентированных на них блокбастеров.</p>
+		<p>Стефани не планирует останавливаться на достигнутом. Не прошло и года после окончания «сумеречного» цикла, как она выпустила книгу <u>«Гостья»</u> – роман заметно более мрачный и направленный на аудиторию постарше. А в дальнейших планах Майер числятся как сиквелы <u>«Гостьи»</u>, так и романы о привидениях, русалках и путешествиях во времени.</p>
+	</div>
+</div>
+
+
+
+<div class="age_small noselect">&nbsp;</div>
+
+<div class="footer noselect"><div class="copy">&copy; 2013 &laquo;<a class="fadeout" href="http://www.westvideo.ru/">Вест Видео</a>&raquo; &nbsp;&nbsp; Создание сайта: <a class="fadeout" href="http://www.kg-design.ru/">КГ-Дизайн</a></div></div>
+
+<div id="counters"><div class="counters_list"><p><?php include ("counters_include.php"); ?></p></div></div>
+
+
+<!-- Index: background-size fix for IE8 -->
+<!--[if lt IE 9]><script src="/js/jquery.backgroundSize.js" type="text/javascript"></script><![endif]-->
+<script type="text/javascript">
+$(function() {
+$("body").css({backgroundSize: "cover"});
+});
+</script>
+
+</body></html>
